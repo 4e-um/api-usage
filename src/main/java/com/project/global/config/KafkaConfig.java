19b9e1a35556169d6313feafc16b719dd862424c
andrew <leegd120@gmail.com>
2026-01-13 14:15:47 +0900

    UPLUS-6 feat : Confluent Kafka 설정 파일 및 초기 설정 구축

---
+++ src/main/java/com/project/global/config/KafkaConfig.java
@@ -0,0 +1,24 @@
+package com.project.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Bean
+    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setAutoStartup(true); // 기본값이 true
+        return factory;
+    }
+}

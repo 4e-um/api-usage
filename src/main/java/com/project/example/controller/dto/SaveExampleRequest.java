6fef801e0a50eaa4d63ed30627f53d5473a60abe
andrew <leegd120@gmail.com>
2026-01-16 00:01:58 +0900

    UPLUS-16 fix : git 충돌 해결

--- src/main/java/com/project/example/controller/dto/SaveExampleRequest.java
+++
@@ -1,3 +0,0 @@
-package com.project.example.controller.dto;
-
-public record SaveExampleRequest(String exampleName, String exampleContent) {}
--- src/main/java/com/project/example/infra/repository/ExampleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.example.infra.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.example.infra.entity.ExampleEntity;
-
-public interface ExampleJpaRepository extends JpaRepository<ExampleEntity, Long> {}

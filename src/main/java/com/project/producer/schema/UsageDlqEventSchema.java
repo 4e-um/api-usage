9bc5c1e3f8f30786e4e6d0f817cbe0539ea35e92
andrew <leegd120@gmail.com>
2026-01-16 00:17:44 +0900

    UPLUS-16 feat : 임시로 작성된 유실 데이터 Producer 코드 제거

--- src/main/java/com/project/producer/schema/UsageDlqEventSchema.java
+++
@@ -1,9 +0,0 @@
-package com.project.producer.schema;
-
-public record UsageDlqEventSchema(
-        String originKey,
-        String originalValue,
-        String error,
-        String failedAt
-) {
-}

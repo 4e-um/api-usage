fdeac3b14762136459cb15d0fa249fe9877809fe
andrew <leegd120@gmail.com>
2026-01-16 00:04:09 +0900

    UPLUS-16 feat : Kafka 시간 처리를 위한 유틸 클래스 별도로 구성

---
+++ src/main/java/com/project/consumer/util/UsageTimeUtil.java
@@ -0,0 +1,35 @@
+package com.project.consumer.util;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class UsageTimeUtil {
+    private static final DateTimeFormatter ISO = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
+    public static String toYyyyMM(String isoTs) {
+        OffsetDateTime odt = OffsetDateTime.parse(isoTs, ISO);
+        return odt.format(DateTimeFormatter.ofPattern("yyyyMM"));
+    }
+
+    public static String toYyyyMMdd(String isoTs) {
+        OffsetDateTime odt = OffsetDateTime.parse(isoTs, ISO);
+        return odt.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+
+    public static long ttlToNextMonthWithBufferSec(String isoTs, int bufferDays) {
+        OffsetDateTime now = OffsetDateTime.parse(isoTs, ISO);
+
+        OffsetDateTime nextMonthStart = now
+                .withDayOfMonth(1)
+                .with(LocalTime.MIDNIGHT)
+                .plusMonths(1);
+
+        Duration base = Duration.between(now, nextMonthStart);
+        Duration buffer = Duration.ofDays(bufferDays);
+
+        return Math.max(
+                base.plus(buffer).getSeconds(),
+                3600
+        );
+    }
+}

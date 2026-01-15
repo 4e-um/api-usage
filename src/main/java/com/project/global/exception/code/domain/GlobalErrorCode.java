e18f1d016d97b19bea2b99ceed21f30b6ed1f36c
andrew <leegd120@gmail.com>
2026-01-16 00:09:48 +0900

    UPLUS-16 fix : git 충돌 해결

--- src/main/java/com/project/global/exception/code/domain/GlobalErrorCode.java
+++ src/main/java/com/project/global/exception/code/domain/GlobalErrorCode.java
@@ -1,9 +1,8 @@
 package com.project.global.exception.code.domain;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
@@ -13,7 +12,9 @@ public enum GlobalErrorCode implements BaseErrorCode {
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_003", "지원하지 않은 Http Method 입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_004", "서버 에러가 발생했습니다."),
     BLOCKED_API(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_005", "운영 환경에서 사용할 수 없는 API 입니다."),
-    JSON_CONVERT_DISABLE(HttpStatus.BAD_REQUEST, "USER_005", "JSON으로 변환하는 과정에서 오류가 발생하였습니다");
+    NOTIFICATION_EVENT_PRODUCE_INVALID(HttpStatus.BAD_REQUEST, "COMMON_006", "Kafka Notification 이벤트 발행 과정에서 에러가 발생했습니다"),
+    PLAN_CHANGE_EVENT_PRODUCE_INVALID(HttpStatus.BAD_REQUEST, "COMMON_007", "Kafka PlanChange 이벤트 발행 과정에서 에러가 발생했습니다"),
+    ;
 
     private final HttpStatus httpStatus;
     private final String customCode;
